f81e0a0df90a9a62462882317732f28fd3d9a412
enver <envereskendarov@gmail.com>
2021-05-04 17:58:59 +0300

    2.3. Загрузка деталей поста. [#389148]

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -1,10 +1,15 @@
 package ru.job4j.html;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import ru.job4j.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * Post - модель данных топика с сайта http://sql.ru/forum/job-offers
+ * Класс {@link Description} - загружает подробное описание объявлений.
  *
  * @author Enver Eskendarov
  * @version 1.0 03/05/2021
@@ -13,11 +18,11 @@ public class Post {
 
     private final String topic;
     private final String link;
-    private final LocalDateTime dateTime;
+    private final LocalDateTime topicDateTime;
 
-    public Post(String topic, LocalDateTime dateTime, String link) {
+    public Post(String topic, LocalDateTime topicDateTime, String link) {
         this.topic = topic;
-        this.dateTime = dateTime;
+        this.topicDateTime = topicDateTime;
         this.link = link;
     }
 
@@ -25,20 +30,54 @@ public class Post {
         return topic;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
+    public LocalDateTime getLocalDateTime() {
+        return topicDateTime;
     }
 
     public String getLink() {
         return link;
     }
 
+    public Description getDescription() {
+        return new Description(link);
+    }
+
     @Override
     public String toString() {
-        final DateTimeFormatter timeFormatter = DateTimeFormatter
-                .ofPattern("EEE, d MMM yy, HH:mm");
-        return String.format("%s (%s) [%s]",
-                topic, dateTime.format(timeFormatter), link
-        );
+        return String.format("%s (%s) [%s]", topic, topicDateTime, link);
+    }
+
+    static class Description {
+
+        private String text;
+        private LocalDateTime localDateTime;
+
+        public Description(String link) {
+            try {
+                final Document doc = Jsoup.connect(link).get();
+                this.text = doc.select(".msgBody").get(1).text();
+                this.localDateTime = new SqlRuDateTimeParser()
+                        .parse(doc.select(".msgFooter")
+                                .first()
+                                .ownText()
+                                .split(" \\[")[0]
+                        );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public LocalDateTime getLocalDateTime() {
+            return localDateTime;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s (%s)", text, localDateTime);
+        }
     }
 }
